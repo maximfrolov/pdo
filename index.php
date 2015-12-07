3a93748c56e582bbb388fa038cb87251ad63906a
Maxim Frolov <consumer2@mail.ru>
2015-12-07 18:24:08 +0500

    создал объект PDO для подключения к базе данных

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
 
+try {
+    $dbh = new PDO(
+        'mysql:dbname=yii2basic;host=127.0.0.1',
+        'root',
+        ''
+    );
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
