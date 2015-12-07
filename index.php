6e3b695f25de68dacf8697422ebcdb17cf9a1ed0
Maxim Frolov <consumer2@mail.ru>
2015-12-07 23:35:02 +0500

    подключил модель Country.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__ . '/models/Country.php';
+
 try {
     $dbh = new PDO('mysql:dbname=yii2basic;host=127.0.0.1', 'root', '', [
     // данная константа используется в том случае, если не знаешь какая кодировка у данных в базе
