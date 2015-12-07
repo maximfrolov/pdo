46a31e8147fdf4d00443c357ab0b492081867298
Maxim Frolov <consumer2@mail.ru>
2015-12-07 20:39:28 +0500

    установил опцию драйверу PDO

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 
 try {
-    $dbh = new PDO(
-        'mysql:dbname=yii2basic;host=127.0.0.1',
-        'root',
-        ''
-    );
+    $dbh = new PDO('mysql:dbname=yii2basic;host=127.0.0.1', 'root', '', [
+    // данная константа используется в том случае, если не знаешь какая кодировка у данных в базе
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    ]);
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
