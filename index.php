a15eb6ff633bcc46e337a6e8a73f0bfff48a7973
Maxim Frolov <consumer2@mail.ru>
2015-12-08 02:39:44 +0500

    исправил баг

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/models/Country.php';
 require_once __DIR__ . '/classes/Connection.php';
 
 try {
-    $dbh = new Connection(include __DIR__ . 'config.php');
+    $dbh = new Connection(include 'config.php');
 
     // подготовленный запрос (метод класса PDO)
     $sth = $dbh->prepare('SELECT * FROM country WHERE code=:code');
