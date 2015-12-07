586b1e9b33bb3988ddfcb2971f61aaccf65635d5
Maxim Frolov <consumer2@mail.ru>
2015-12-08 02:42:59 +0500

    все детали реализации подключения и соединения с базой данных спрятал в класс Connection

--- classes/Connection.php
+++ classes/Connection.php
@@ -4,6 +4,20 @@ class Connection
     extends PDO
 {
 
-
+    public function __construct($config)
+    {
+        $dsn = $config['driver'] . ':dbname=' . $config['dbname'] . ';host=' . $config['host'];
+        parent::__construct(
+            $dsn,
+            $config['user'],
+            $config['password'],
+            // данная константа используется в том случае, если не знаешь какая кодировка у данных в базе
+            [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',]
+        );
+        // установка атрибута: режим ошибок, со значением: выброса исключений
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // установка атрибута: режим выборки данных по умолчанию, со значением: объект класса stdClass
+        $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    }
 
 }
