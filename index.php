f9b554fe40933be6131676548834420a0dfbc6b4
Maxim Frolov <consumer2@mail.ru>
2015-12-08 00:12:58 +0500

    изменил тип объекта с PDO на Connection, в строке DSN

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/models/Country.php';
 
 try {
-    $dbh = new PDO('mysql:dbname=yii2basic;host=127.0.0.1', 'root', '', [
+    $dbh = new Connection('mysql:dbname=yii2basic;host=127.0.0.1', 'root', '', [
     // данная константа используется в том случае, если не знаешь какая кодировка у данных в базе
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
     ]);
