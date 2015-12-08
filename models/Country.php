c578566a9c71388c77ceb7550d82244810d6e4f6
Maxim Frolov <consumer2@mail.ru>
2015-12-09 00:04:49 +0500

    вынес метод findAll() в абстрактную модель Model + унаследовал Country от Model + подключил Model

--- models/Country.php
+++ models/Country.php
@@ -1,21 +1,15 @@
 <?php
 
-require_once __DIR__ . '/../classes/Connection.php';
+require_once __DIR__ . '/Model.php';
 
 class Country
+    extends Model
 {
+
     const TABLE = 'country';
 
     public $code;
     public $name;
     public $population;
-
-    public static function findAll()
-    {
-        $dbh = new Connection();
-        $sth = $dbh->prepare('SELECT * FROM ' . self::TABLE);
-        $sth->execute();
-        return $sth->fetchAll(PDO::FETCH_CLASS, self::class);
-    }
 
 }
