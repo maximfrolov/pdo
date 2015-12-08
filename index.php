d35f629b8d8cc8077a75429bc8c59157e5227402
Maxim Frolov <consumer2@mail.ru>
2015-12-09 00:18:18 +0500

    подключил модель Users + вызвал у нее метод findAll() + убрал лишний код

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
 <?php
 
 require_once __DIR__ . '/models/Country.php';
+require_once __DIR__ . '/models/Users.php';
 
 try {
-    /*$dbh = new Connection();
 
-    // подготовленный запрос (метод класса PDO)
-    $sth = $dbh->prepare('SELECT * FROM country WHERE code=:code');
-    // привязка параметра к заданному значению, с явно заданным типом данных этого значения
-    $sth->bindValue(':code', 'RU', PDO::PARAM_STR);
-    // запуск подготовленного запроса (метод класса PDOStatement)
-    $sth->execute();
-    // возврат результата запроса (метод класса PDOStatement)
-    $res = $sth->fetchAll(PDO::FETCH_CLASS, Country::class);*/
     $data = Country::findAll();
+    var_dump($data);
+    $data = Users::findAll();
     var_dump($data);
 
 } catch (PDOException $e) {
