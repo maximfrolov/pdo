6ee51ea901a8ae816ede6084a1e38dbc3f268632
Maxim Frolov <consumer2@mail.ru>
2015-12-08 21:24:25 +0500

    добавил метод findAll() и константу TABLE

--- models/Country.php
+++ models/Country.php
@@ -2,9 +2,17 @@
 
 class Country
 {
+    const TABLE = 'country';
 
     public $code;
     public $name;
     public $population;
+
+    public static function findAll()
+    {
+        $dbh = new Connection();
+        $sth = $dbh->prepare('SELECT * FROM' . self::TABLE);
+        return $sth->fetchAll(PDO::FETCH_CLASS, self::class);
+    }
 
 }
