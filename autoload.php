4a3827e8550c08faefb74904acf37c2654b7c184
Maxim Frolov <consumer2@mail.ru>
2015-12-11 12:39:12 +0500

    переписал функцию автозагрузки

--- autoload.php
+++ autoload.php
@@ -2,6 +2,11 @@
 
 spl_autoload_register(function($class)
 {
-    require __DIR__ . '\\' . $class . '.php';
-
+    if(preg_match('/\\\\/', $class)) {
+        $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    }
+    $fileName = __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
+    if(file_exists($fileName)) {
+        require_once $fileName;
+    }
 });
